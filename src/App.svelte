f9530251bd497cb798dea48b7974f13126a2d361
raad1masum <piraadwan@gmail.com>
2020-07-24 13:45:06 -0700

    create new note button

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
 
 <main>
     <div style="color: inherit; fill: inherit;"><div style="display: flex;"><div contenteditable="true" spellcheck="true" placeholder="Type '/' for commands" data-root="true" style="max-width: 100%; width: 100%; white-space: pre-wrap; word-break: break-word; caret-color: rgb(55, 53, 47); padding: 3px 2px;"></div></div></div>
+    <button type="button" onclick="newNote">add note</button>
 </main>
 
 <style>
