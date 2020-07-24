9cd32d053d6248eaaa9b5914c775319bc277a778
raad1masum <piraadwan@gmail.com>
2020-07-24 13:52:21 -0700

    create note container

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,10 @@
 </script>
 
 <main>
-    <div style="color: inherit; fill: inherit;"><div style="display: flex;"><div contenteditable="true" spellcheck="true" placeholder="Type '/' for commands" data-root="true" style="max-width: 100%; width: 100%; white-space: pre-wrap; word-break: break-word; caret-color: rgb(55, 53, 47); padding: 3px 2px;"></div></div></div>
-    <button type="button" onclick="newNote">add note</button>
+    <div class="note-container">
+        <div style="color: inherit; fill: inherit;"><div style="display: flex;"><div contenteditable="true" spellcheck="true" placeholder="Type '/' for commands" data-root="true" style="max-width: 100%; width: 100%; white-space: pre-wrap; word-break: break-word; caret-color: rgb(55, 53, 47); padding: 3px 2px;"></div></div></div>
+    </div>
+    <button type="button" on:click={newNote}>add note</button>
 </main>
 
 <style>
