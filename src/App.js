6bc53bf7ea13f1b764111f3fdd20cb6e051c9b62
raad1masum <piraadwan@gmail.com>
2020-07-24 15:00:01 -0700

    add js

--- src/App.js
+++ src/App.js
@@ -1,2 +1,24 @@
+export let notes = []
+
+var container = document.getElementById("control");
+
+function newNote() {
+    notes = notes.concat("");
+    console.log("worked")
+}
+
+function removeNote(index) {
+    var key = event.keyCode || event.charCode;
+    console.log(notes[index].length)
+
+    if( key == 8 || key == 46 && notes[index].length == 0) {
+        delete notes[index];
+        console.log("worked2")
+    }
+}
+
+function onNoteChange(index) {
+    notes[index] = event.target.value
+}
 
 export { newNote, onNoteChange }
